a137393a123c4126181d0b156335fdcbc4a76e24
chefpino <pino.ficara@gmail.com>
2020-05-05 13:51:17 +0200

    ok

---
+++ data.js
@@ -0,0 +1,91 @@
+/*
+var initAngle=pi/2;      //trunk angle
+var dAngle=pi/8;         //half of bifurcation angle, pi/8 nice
+var initLen=150;         //trunk length max 150
+var minLen=30;           //20, 25 nice
+var reduction=0.8;       //0.8 nice
+var initTrunk=20;        //trunk initial thickness
+var trunkReduction=0.5;  //trunk => tree reduction factor 
+var minBranch=0.1;       // branches min thickness
+*/
+
+var params = {
+  "initAngle":{
+     "val":Math.PI/2,
+     "min":0,
+     "max":Math.PI,
+     "default":Math.PI/2,
+     "label":"trunk angle",
+     "minLabel":"0",
+     "maxLabel":"π"
+     },
+   "dAngle":{
+     "val":Math.PI/8,
+     "min":Math.PI/8,
+     "max":Math.PI/4,
+     "default":Math.PI/8,
+     "label":"branch angle",
+     "minLabel":"π/8",
+     "maxLabel":"π/4"
+     },
+   "initLen":{
+     "val":150,
+     "min":25,
+     "max":150,
+     "default":150,
+     "label":"trunk initial height",
+     "minLabel":"25",
+     "maxLabel":"150"
+
+     },
+   "minLen":{
+     "val":20,
+     "min":15,
+     "max":40,
+     "default":20,
+     "label":"branch min length",
+     "minLabel":"15",
+     "maxLabel":"40"
+
+     },
+   "reduction":{
+     "val":0.8,
+     "min":0.5,
+     "max":0.8,
+     "default":0.8,
+     "label":"branch reduction factor",
+     "minLabel":"0.5",
+     "maxLabel":"0.8"
+
+     },
+   "initTrunk":{
+     "val":20,
+     "min":1,
+     "max":50,
+     "default":20,
+     "label":"trunk initial thickness",
+     "minLabel":"1",
+     "maxLabel":"50"
+
+     },
+   "trunkReduction":{
+     "val":0.5, 
+     "min":0.1,
+     "max":0.9,
+     "default":0.5,
+     "label":"trunk to branch reduction factor",
+     "minLabel":"0.1",
+     "maxLabel":"0.9"
+     },
+   "minBranch":{
+     "val":0.1,
+     "min":0.1,
+     "max":1,
+     "default":0.1,
+     "label":"branch min thickness",
+     "minLabel":"0.1",
+     "maxLabel":"1"
+
+     }
+
+}
--- interface.js
+++ interface.js
@@ -1,94 +1,4 @@
-/*
-var initAngle=pi/2;      //trunk angle
-var dAngle=pi/8;         //half of bifurcation angle, pi/8 nice
-var initLen=150;         //trunk length max 150
-var minLen=30;           //20, 25 nice
-var reduction=0.8;       //0.8 nice
-var initTrunk=20;        //trunk initial thickness
-var trunkReduction=0.5;  //trunk => tree reduction factor 
-var minBranch=0.1;       // branches min thickness
-*/
 
-var params = {
-  "initAngle":{
-     "val":Math.PI/2,
-     "min":0,
-     "max":Math.PI,
-     "default":Math.PI/2,
-     "label":"trunk angle",
-     "minLabel":"0",
-     "maxLabel":"π"
-     },
-   "dAngle":{
-     "val":Math.PI/8,
-     "min":Math.PI/8,
-     "max":Math.PI/4,
-     "default":Math.PI/8,
-     "label":"branch angle",
-     "minLabel":"π/8",
-     "maxLabel":"π/4"
-     },
-   "initLen":{
-     "val":150,
-     "min":25,
-     "max":150,
-     "default":150,
-     "label":"trunk initial height",
-     "minLabel":"25",
-     "maxLabel":"150"
-
-     },
-   "minLen":{
-     "val":20,
-     "min":15,
-     "max":40,
-     "default":20,
-     "label":"branch min length",
-     "minLabel":"15",
-     "maxLabel":"40"
-
-     },
-   "reduction":{
-     "val":0.8,
-     "min":0.5,
-     "max":0.8,
-     "default":0.8,
-     "label":"branch reduction factor",
-     "minLabel":"0.5",
-     "maxLabel":"0.8"
-
-     },
-   "initTrunk":{
-     "val":20,
-     "min":1,
-     "max":50,
-     "default":20,
-     "label":"trunk initial thickness",
-     "minLabel":"1",
-     "maxLabel":"50"
-
-     },
-   "trunkReduction":{
-     "val":0.5, 
-     "min":0.1,
-     "max":0.9,
-     "default":0.5,
-     "label":"trunk to branch reduction factor",
-     "minLabel":"0.1",
-     "maxLabel":"0.9"
-     },
-   "minBranch":{
-     "val":0.1,
-     "min":0.1,
-     "max":1,
-     "default":0.1,
-     "label":"branch min thickness",
-     "minLabel":"0.1",
-     "maxLabel":"1"
-
-     }
-
-}
 
 function paramToSlider(strParam,iValue){  
 var x1=1*params[strParam].min;
@@ -103,3 +13,64 @@ var x2=1*params[strParam].max;
 return x1+iValue*(x2-x1)/100;
 //return 1*x1;
 }
+
+//------------ LEAF ----------------------------------------
+function leaf(x,y,l,alpha,beta){
+  /*
+  x,y coordinate beginning leaf
+  l: length of leaf
+  alpha: angle of the leaf simmetry line
+  b: angle of the arc describing the leaf 
+  */
+  
+  //try changing alpha a little bit to make leaves more natural
+  if (alpha<pi/2) {
+    alpha=alpha-Math.random();
+  } else {
+    alpha=alpha+Math.random();
+  }
+
+
+  var r=l/(2*Math.sin(beta/2));
+  var gamma=pi/2-beta/2-alpha;
+  var xc=0;
+  var yc=0;
+  
+
+
+ // leaf first half ----------------------------
+    
+  //calc center
+  xc=x+r*Math.cos(gamma);
+  yc=y-r*Math.sin(gamma);
+  
+  //leaf'simmetry line
+  ctx.beginPath();
+  ctx.strokeStyle = "darkgreen";
+
+  ctx.moveTo(px(x),py(y));
+  ctx.lineTo(px(x+l*Math.cos(alpha)),py(y+l*Math.sin(alpha)));
+  ctx.stroke(); 
+  
+  //--- leaf 1st arc
+  ctx.arc(px(xc),py(yc), r, 2*pi-(pi-gamma-beta),2*pi-(pi-gamma),true);
+  ctx.fillStyle = "yellow";
+  ctx.fill();
+  ctx.stroke();
+  
+  // leaf second  half ----------------------------
+  
+  //calc center
+  var diagonal=Math.sqrt(r*r-l*l/4)
+  xc=xc-2*diagonal*Math.cos(gamma+beta/2);
+  yc=2*diagonal*Math.sin(gamma+beta/2)+yc;
+
+  ctx.beginPath();
+  ctx.arc(px(xc),py(yc), r, -1*(alpha+beta/2)+pi/2,-1*(alpha-beta/2)+pi/2,false);
+  ctx.fillStyle = "lightgreen";
+  ctx.fill();
+  ctx.stroke();  
+}
+
+
+

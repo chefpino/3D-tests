baad5ce52d9bc11561221b70d1b9377defcfb371
chefpino <pino.ficara@gmail.com>
2020-05-20 08:24:56 +0200

    morning commit

--- navigation/menu.js
+++ navigation/menu.js
@@ -2,95 +2,117 @@
 WIP
 this can be a page containing menu/navigation global variables
 in form of an object/json file
+
+WIP: class="vertical-menu"
 */
 
 var navigation = {
-  "home":{
-     "id":"home",
-     "link":"https://chefpino.github.io/fun/",
-     "label":"HOME",
-     "hasChildren":true,
-     "children":{
-                 "item1":"fractals",
-                 "item2":"plotmathfunctions"
-                },
-     "visible":true 
-     },
-        "fractals":{
-          "id":"fractals",
-          "link":"https://chefpino.github.io/fun/fractals/",
-          "label":"FRACTALS",
-          "hasChildren":true,
-          "children":{
-            "item1":"mandelbrot",
-            "item2":"burningship",
-            "item3":"tree"
-          }
-        },
-              "mandelbrot":{
-                "id":"mandelbrot",
-                "link":"https://chefpino.github.io/fun/fractals/mandelbrot/index.html",
-                "label":"MANDELBROT",
-                "hasChildren":false
-              },
-              "burningship":{
-                "id":"burningship",
-                "link":"https://chefpino.github.io/fun/fractals/burningship/index.html",
-                "label":"BURNING SHIP",
-                "hasChildren":false
-              },
-              "tree":{
-                "id":"tree",
-                "link":"https://chefpino.github.io/fun/fractals/tree/index.html",
-                "label":"TREE",
-                "hasChildren":false
-              },
-
-        "plotmathfunctions":{
-          "id":"plotmathfunctions",
-          "link":"https://chefpino.github.io/fun/plothmathfunctions/",
-          "label":"PLOT FUNCTIONS",
-          "hasChildren":false
-        }
-
-}
+  home: {
+    id: "home",
+    link: "https://chefpino.github.io/fun/",
+    label: "HOME",
+    hasChildren: true,
+    children: {
+      item1: "fractals",
+      item2: "plotmathfunctions",
+    },
+    visible: true,
+  },
+  fractals: {
+    id: "fractals",
+    link: "#",
+    label: "FRACTALS",
+    hasChildren: true,
+    children: {
+      item1: "mandelbrot",
+      item2: "burningship",
+      item3: "tree",
+      item4: "unitcircle",
+    },
+  },
+  mandelbrot: {
+    id: "mandelbrot",
+    link: "https://chefpino.github.io/fun/fractals/mandelbrot/index.html",
+    label: "MANDELBROT",
+    hasChildren: false,
+  },
+  burningship: {
+    id: "burningship",
+    link: "https://chefpino.github.io/fun/fractals/burningship/index.html",
+    label: "BURNING SHIP",
+    hasChildren: false,
+  },
+  tree: {
+    id: "tree",
+    link: "https://chefpino.github.io/fun/fractals/tree/index.html",
+    label: "TREE",
+    hasChildren: false,
+  },
+  unitcircle: {
+    id: "unitcircle",
+    link: "https://chefpino.github.io/fun/fractals/unitcircle/index.html",
+    label: "UNIT CIRCLE",
+    hasChildren: false,
+  },
+  plotmathfunctions: {
+    id: "plotmathfunctions",
+    link: "#",
+    label: "PLOT FUNCTIONS",
+    hasChildren: true,
+    children: {
+      item1: "twoD",
+      item2: "threeD",
+      item3: "complex",
+    },
+  },
+  twoD: {
+    id: "twoD",
+    link: "https://chefpino.github.io/fun/plothmathfunctions/2d.html",
+    label: "2D",
+    hasChildren: false,
+  },
+  threeD: {
+    id: "threeD",
+    link: "https://chefpino.github.io/fun/plothmathfunctions/index.html",
+    label: "3D",
+    hasChildren: false,
+  },
+  complex: {
+    id: "complex",
+    link:
+      "https://chefpino.github.io/fun/plothmathfunctions/rotationscomplexplane.html",
+    label: "Rotations (WIP)",
+    hasChildren: false,
+  },
+};
 
 //----------------------------------------------------------------------------------
 
-function genMenu(strKey,nestinglevel){
-
-  var strTemp="";
+function genMenu(strKey, nestinglevel) {
+  var strTemp = "";
   var oChildren; //object containing children
-  var strTempKey="";
+  var strTempKey = "";
 
   //temporary solution for display only, will improve later
- 
-  strTemp=  '<a href="'+ navigation[strKey].link + '">'; 
+
+  strTemp = '<a href="' + navigation[strKey].link + '">';
   strTemp += navigation[strKey].label; //label only
-  strTemp += '</a>';
+  strTemp += "</a>";
 
-  if (navigation[strKey].hasChildren==true){
-
+  if (navigation[strKey].hasChildren == true) {
     //if it has children list them here
-    oChildren=navigation[strKey].children;
+    oChildren = navigation[strKey].children;
 
     for (const property in oChildren) {
-
-      strTempKey=oChildren[property];
-      strTemp += tirets(nestinglevel+1) + genMenu(strTempKey,nestinglevel+1); //used to be <BR>+
-
+      strTempKey = oChildren[property];
+      strTemp +=
+        "<br>" + tirets(nestinglevel + 1) + genMenu(strTempKey, nestinglevel + 1); //used to be <BR>+
     }
   }
 
   return strTemp;
-
 }
 //-----------------------------------------
 function tirets(n) {
-  return "-".repeat(2*n);
+  return "&nbsp;".repeat(2*n);
 }
-
-
-
-
-

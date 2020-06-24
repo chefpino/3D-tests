70b052e4b58d4ae0587a4c166098015ed295fa07
chefpino <pino.ficara@gmail.com>
2020-06-24 21:10:10 +0200

    end

--- plotmathfunctions/2d/2d-library.js
+++ plotmathfunctions/2d/2d-library.js
@@ -1,5 +1,6 @@
 function listFunctions() {
   var strTable = "<table border=1>";
+  var i=0;
 
   for (const key in params.functions) {
     if (params.functions.hasOwnProperty(key)) {
@@ -9,12 +10,38 @@ function listFunctions() {
           strTable += "<tr>";
           strTable += "<td>" + key + "</td>";
           strTable += "<td> " + params.functions[key] + "</td>";
-          strTable += "<td>" + " delete " + "</td>";
+          strTable += "<td>" + `<a href="#" onclick="return deleteFunction('${key}')"> delete </a>` + "</td>";
           strTable += "</tr>";
+          i++;
       }
     }
   }
+  //add new function 
+  i++;
+  strTable += "<tr>";
+          strTable += "<td> f" + i + "</td>";
+          strTable += "<td><input type='text' id='newF'></td>";
+          strTable += "<td>" + `<a href="#" onclick="addFunction('f${i}')"> add </a>` + "</td>";
+          strTable += "</tr>";
+
   strTable += "</table>";
 
   return strTable;
+}
+
+function deleteKey(obj,key){
+  delete obj[key];
+}
+
+//wrapper for functions
+function deleteFunction(key){
+  deleteKey(params.functions,key);
+  displayFunctions();
+  loadvaluesandgo(); 
+}
+
+function addFunction(key){
+  params.functions[key]=document.getElementById("newF").value;
+  displayFunctions();
+  loadvaluesandgo(); 
 }

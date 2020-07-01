d98671de73435446c34bfd82c66ce526ea415b21
chefpino <pino.ficara@gmail.com>
2020-07-01 10:45:45 +0200

    tests

---
+++ fractals/fractals-data/modules.data.js
@@ -0,0 +1,31 @@
+var params = {
+  "module": {
+    default: 10,
+    val: 10,
+    min: 2,
+    max: 200,
+    integer: true,
+    visible: true,
+    "label":"mod",
+    "minLabel":"2",
+    "maxLabel":"200",
+
+  },
+  "factor":{
+    default: 2,
+    val: 2,
+    min: 2,
+    max: 100,
+    integer: false,
+    visible: true,
+    "label":"factor",
+    "minLabel":"2",
+    "maxLabel":"100",
+
+  },
+     "fixedOrigin":{
+        val: false,
+        visible: false
+         }
+    }
+  
--- navigation/menu.js
+++ navigation/menu.js
@@ -1,4 +1,3 @@
-
 var navigation = {
   home: {
     id: "home",
@@ -21,7 +20,7 @@ var navigation = {
       item2: "julia",
       item3: "burningship",
       item4: "tree",
-      item5: "barnsley"
+      item5: "barnsley",
     },
   },
   mandelbrot: {
@@ -29,14 +28,16 @@ var navigation = {
     link: "../../fractals/mandelbrot/index.html",
     label: "Mandelbrot",
     hasChildren: false,
-    content: "Click on the fractal to zoom.<br>The red vectors that follow the mouse pointer represent the path of the fractal function that either converges to a finite point or escapes to infinite."
+    content:
+      "Click on the fractal to zoom.<br>The red vectors that follow the mouse pointer represent the path of the fractal function that either converges to a finite point or escapes to infinite.",
   },
   julia: {
     id: "julia",
     link: "../../fractals/julia/index.html",
     label: "Julia",
     hasChildren: false,
-    content: "Click on the fractal to zoom.<br>The red vectors that follow the mouse pointer represent the path of the fractal function that either converges to a finite point or escapes to infinite."
+    content:
+      "Click on the fractal to zoom.<br>The red vectors that follow the mouse pointer represent the path of the fractal function that either converges to a finite point or escapes to infinite.",
   },
   burningship: {
     id: "burningship",
@@ -50,22 +51,30 @@ var navigation = {
     link: "../../fractals/tree/index.html",
     label: "Tree",
     hasChildren: false,
-    content: "Change the parameters to generate different fractal trees."
+    content: "Change the parameters to generate different fractal trees.",
   },
   barnsley: {
     id: "barnsley",
     link: "../../fractals/barnsley/index.html",
     label: "Barnsley",
     hasChildren: false,
-    content: "Change the parameter to increase number of points."
+    content: "Change the parameter to increase number of points.",
   },
   unitcircle: {
     id: "unitcircle",
     link: "../../plotmathfunctions/unitcircle/index.html",
     label: "Unit Circle",
     hasChildren: false,
-    content: "z -> z^2"
+    content: "z -> z^2",
   },
+  modules: {
+    id: "modules",
+    link: "../../plotmathfunctions/unitcircle/wip-modules.html",
+    label: "Modules (WIP)",
+    hasChildren: false,
+    content: "under constructions",
+  },
+
   plotmathfunctions: {
     id: "plotmathfunctions",
     link: "#",
@@ -76,6 +85,7 @@ var navigation = {
       item1: "threeD",
       item2: "complex",
       item3: "unitcircle",
+      item4: "modules"
     },
   },
   twoD: {
@@ -83,49 +93,47 @@ var navigation = {
     link: "../../plotmathfunctions/2d/index.html",
     label: "Functions y = f(x)",
     hasChildren: false,
-    content: "Click on the graph to zoom. Page under construction"
+    content: "Click on the graph to zoom. Page under construction",
   },
   threeD: {
     id: "threeD",
     link: "../../plotmathfunctions/3d/index.html",
     label: "Functions z = f(x,y)",
     hasChildren: false,
-    content: "Page under construction"
+    content: "Page under construction",
   },
   complex: {
     id: "complex",
-    link:
-      "../../plotmathfunctions/rotations/index.html",
+    link: "../../plotmathfunctions/rotations/index.html",
     label: "Rotations",
     hasChildren: false,
-    content: "Click on the complex plane to draw a polygon.<br>Double click to choose a vector.<br>Apply a rotation or a translation.<br>Auto applies the rotation until polygon becomes smaller than a pixel or larger than the canvas."
+    content:
+      "Click on the complex plane to draw a polygon.<br>Double click to choose a vector.<br>Apply a rotation or a translation.<br>Auto applies the rotation until polygon becomes smaller than a pixel or larger than the canvas.",
   },
 };
 
 //----------------------------------------------------------------------------------
 // -- this is a recursive function to generate the menu, it can be improved
 function genMenu(strKey, nestinglevel, strCurrentPage) {
-  var strTemp = ((nestinglevel == 0) ? "<table width='50%'>" :  "");
+  var strTemp = nestinglevel == 0 ? "<table width='50%'>" : "";
   var oChildren; //object containing children
-  
 
-  var strLabel=navigation[strKey].label;
-  var strLink=navigation[strKey].link;
-  
+  var strLabel = navigation[strKey].label;
+  var strLink = navigation[strKey].link;
 
   //highlight if current page (puts it in bold, but can be improved)
-    if (strCurrentPage==strKey){
-      strLabel = "<b>" + strLabel + "</b>";
+  if (strCurrentPage == strKey) {
+    strLabel = "<b>" + strLabel + "</b>";
   }
-  
+
   //if it's a link add link tags
-  if (navigation[strKey].link!="#"){
-    strLabel = '<a href="' + strLink + '">' + strLabel + "</a>"
+  if (navigation[strKey].link != "#") {
+    strLabel = '<a href="' + strLink + '">' + strLabel + "</a>";
   }
 
+  strTemp =
+    strTemp + "<tr><td>" + indent(nestinglevel) + strLabel + "</td></tr>";
 
-  strTemp = strTemp + "<tr><td>" + indent(nestinglevel) + strLabel + "</td></tr>";
-  
   if (navigation[strKey].hasChildren == true) {
     //if it has children list them here
     oChildren = navigation[strKey].children;
@@ -133,27 +141,31 @@ function genMenu(strKey, nestinglevel, strCurrentPage) {
     for (const property in oChildren) {
       strTempKey = oChildren[property];
       //strTemp += "<tr><td>";
-      strTemp += genMenu(strTempKey, nestinglevel + 1,strCurrentPage); //used to be <BR>+
+      strTemp += genMenu(strTempKey, nestinglevel + 1, strCurrentPage); //used to be <BR>+
       //strTemp += genMenu(strTempKey, nestinglevel + 1,strCurrentPage); //used to be <BR>+
       //strTemp += "</td></tr>";
     }
   }
-  strTemp += ((nestinglevel == 0) ? "</table>" :  "");
+  strTemp += nestinglevel == 0 ? "</table>" : "";
   return strTemp;
 }
 
 function genNavigation(strCurrentPage) {
-  document.getElementById("menu").innerHTML=genMenu("home",0,strCurrentPage); //add navigation
-  document.title=navigation[strCurrentPage].label;
-  document.getElementById("content").innerHTML = 
-    "<b>" + navigation[strCurrentPage].label + "</b><br>" + 
+  document.getElementById("menu").innerHTML = genMenu(
+    "home",
+    0,
+    strCurrentPage
+  ); //add navigation
+  document.title = navigation[strCurrentPage].label;
+  document.getElementById("content").innerHTML =
+    "<b>" +
+    navigation[strCurrentPage].label +
+    "</b><br>" +
     navigation[strCurrentPage].content;
-  
 }
-
 
 //-----------------------------------------
 // just spaces for indentation
 function indent(n) {
-  return "&nbsp;".repeat(5*n);
+  return "&nbsp;".repeat(5 * n);
 }

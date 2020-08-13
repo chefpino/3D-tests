a891b46f7225eba7fc5d46bad5438cc679134a0d
chefpino <pino.ficara@gmail.com>
2020-08-13 15:27:31 +0200

    imgs up and down

--- navigation/controls.js
+++ navigation/controls.js
@@ -57,7 +57,7 @@ function generateControls() {
               `' onchange='updateParam(this.id);${recalc}' >`
           );
           strHTML = strHTML + td(params[key].maxLabel);
-          strHTML += td(`<a href="#" onclick="upAndDown('${key}',1);">&uarr;</a><br><a href="#" onclick="upAndDown('${key}',-1);">&darr;</a>`);
+          strHTML += td(`<a href="#" onclick="upAndDown('${key}',1);"><img src="https://chefpino.github.io/fun/imgs/arrowUp.jpg" width=15 height=15 "></a><br><a href="#" onclick="upAndDown('${key}',-1);"><img src="https://chefpino.github.io/fun/imgs/arrowDown.jpg" width=15 height=15 "></a>`);
           strHTML +=
           td(`<div id="val_${key}">${Math.round(params[key].default*1000)/1000}</div>`,65);
           break;
